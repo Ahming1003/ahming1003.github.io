a65a2ca6f4d26c8657fb08054a8286dd5c7ed797
Ahming1003 <102003784+Ahming1003@users.noreply.github.com>
2026-03-07 11:10:39 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("hello world") ;
+
+ function toggleTheme() {
+
+    const html = document.documentElement ;
+
+    const current_theme = html.getAttribute("data-theme") ;
+
+    const newTheme = current_theme === "dark" ? "light" : "dark" ;
+
+    html.setAttribute("data-theme", newTheme) ;
+}
+
+// 簡單的進入動畫
+document.addEventListener("DOMContentLoaded", () => {
+
+    const sections = document.querySelectorAll("section") ;
+
+    sections.forEach((s, index) => {
+        s.style.opacity = "0";
+        s.style.transform = "translateY(20px)";
+        setTimeout(() => {
+                s.style.opacity = "1";
+                s.style.transform = "translateY(0)";
+            }, 
+            200 * index
+        );
+    });
+});
